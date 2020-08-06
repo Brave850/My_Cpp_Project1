9a75160fefc7b39b2af65408ad869057ca922c73
Brave850 <67492957+Brave850@users.noreply.github.com>
2020-08-06 19:55:06 +0530

    Create Product.cpp
    This program prints products of two floating number

---
+++ Product.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+
+{
+
+  float a,b;
+  
+  cin>>a>>b;
+  
+  float product=a*b;
+  
+  printf("%0.1f",product);
+  
+   return 0;
+}
